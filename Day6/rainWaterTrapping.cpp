89be9b8401697ac0980f35ce1b53ce5b8a9650b0
Himanshu Negi <himanshunegi3295@gmail.com>
2019-12-25 18:04:45 +0530

    rainWaterProblem code

---
+++ Day6/rainWaterTrapping.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int arr[]={0,2,1,3,0,1,2,1,2,1};
+	int n=10,sum;
+	int l[n], r[n]={0};
+	
+	for(int i=0;i<n;i++)
+	{
+		l[i]=r[i]=arr[i];
+		
+	}
+	for(int i=1;i<n;i++)
+	{
+		if(l[i-1]>l[i])
+		{
+			l[i]=l[i-1];
+		}
+		else
+		{
+			l[i]=arr[i];
+		}
+		
+	}
+	
+	for(int i=n-2;i>=0;i--)
+	{
+		if(r[i]>r[i+1])
+		{
+			r[i]=r[i];
+		}
+		else
+		{
+			r[i]=r[i+1];
+		}
+	}
+	
+	for(int i=0;i<n;i++)
+	{
+		sum+=min(l[i],r[i])-arr[i];
+	}
+	cout<<sum<<endl;
+}
