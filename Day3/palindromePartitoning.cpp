4a1776d20c58f4465cd696b4c50c103cf9f83f05
Himanshu-Negi8 <35424478+Himanshu-Negi8@users.noreply.github.com>
2020-08-26 12:17:11 +0530

    Update palindromePartitoning.cpp

--- Day3/palindromePartitoning.cpp
+++ Day3/palindromePartitoning.cpp
@@ -1,32 +1,46 @@
 #include<iostream>
 using namespace std;
 
-bool isPalindrome(string str,int left,int right)
-{
-	while(left<=right)
-	if(str[left]!=str[right])
-	{
-		return false;	
+bool isPalindrome(string str,int left,int right){
+	while(left<=right){
+		if(str[left]!=str[right]){
+			return false;
+		}
+		left++;
+		right--;
 	}
-	left++;
-	right--;
+
+	return true;
 }
 
-int palindromePartitioning(string str,int start,int end)
-{
-	if(start==end)
-	{
+int palindromePartitioning(string str,int start,int end){
+	if(start>end){
 		return 0;
 	}
-	if (start>end)
-	{
+
+	if(start==end){
 		return 0;
 	}
-	
-	for(int i=0;i<=end;i++)
-	{
-		int leftCuts = palindromePartitoniing(str,)
+
+	if(end-start ==1){
+		return str[start]==str[end] ? 0 : 1;
 	}
+
+	if(isPalindrome(str,start,end)){
+		return 0;
+	}
+
+	int minValue = INT_MAX;
+
+	for(int i=start;i<end;i++){
+
+		int leftCuts = palindromePartitioning(str,start,i);
+		int rightCuts = palindromePartitioning(str,i+1,end);
+
+		minValue = min(minValue,leftCuts + 1 + rightCuts);
+	}
+
+	return minValue;
 }
 
 
